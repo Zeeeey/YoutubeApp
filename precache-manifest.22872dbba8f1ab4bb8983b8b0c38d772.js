49521084576391c3dde8dd1838125be64eff6631
Ezinne Ahumibe <ahumibee@Ezinnes-MacBook-Pro.local>
2022-01-16 22:07:05 +0100

    Updates

---
+++ precache-manifest.22872dbba8f1ab4bb8983b8b0c38d772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c909cd4d1d7c7412046cabb81109c6",
+    "url": "/YoutubeApp/index.html"
+  },
+  {
+    "revision": "1dc4f028ce4cb8af47e8",
+    "url": "/YoutubeApp/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "dccb36376c85a5899500",
+    "url": "/YoutubeApp/static/css/main.36b8e7cd.chunk.css"
+  },
+  {
+    "revision": "1dc4f028ce4cb8af47e8",
+    "url": "/YoutubeApp/static/js/2.b39be7ed.chunk.js"
+  },
+  {
+    "revision": "dccb36376c85a5899500",
+    "url": "/YoutubeApp/static/js/main.b2f89bb3.chunk.js"
+  },
+  {
+    "revision": "8b9573489e14d1af8938",
+    "url": "/YoutubeApp/static/js/runtime-main.07bc411c.js"
+  }
+]);
